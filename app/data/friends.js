7c82d4a0439ea546ea51120ba1162586bc33ef32
Mad Marine <pnnroman92@gmail.com>
2019-07-09 15:55:21 -0500

    completed the project and added new friends and got the list working

---
+++ app/data/friends.js
@@ -0,0 +1,82 @@
+var friendsArray = [
+	{
+		name: 'Naked Snake',
+		photo: 'https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwiD5rqO16jjAhVNdt8KHXB-AaoQjRx6BAgBEAQ&url=https%3A%2F%2Fwww.abbeysupply.com%2Fresources%2Fkit-list-naked-snake-metal-gear&psig=AOvVaw08j5HcBdDatilFMplCmnXh&ust=1562790666531369',
+		scores: [
+			'10',
+			'10',
+			'10',
+			'10',
+			'10'
+		]
+	},
+	{
+		name: 'Venom',
+		photo: 'https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwj6joHp16jjAhVic98KHcHbB4cQjRx6BAgBEAQ&url=https%3A%2F%2Fwww.1999.co.jp%2Feng%2F10348243&psig=AOvVaw2ghA_AarMRCVesWNnurH6o&ust=1562790853180639',
+		scores: [
+			'8',
+			'8',
+			'8',
+			'8',
+			'8'
+		]
+	},
+	{
+		name: 'Solid Snake',
+		photo: 'https://static.tvtropes.org/pmwiki/pub/images/Solid_2_295.jpg',
+		scores: [
+			'10',
+			'6',
+			'9',
+			'7',
+			'8'
+		]
+	},
+	{
+		name: 'Old Snake',
+		photo: 'https://vignette.wikia.nocookie.net/vsbattles/images/6/6d/Metal-gear-png-metal-gear-transparent-image-png-image-745.png/revision/latest?cb=20190504203111',
+		scores: [
+			'7',
+			'7',
+			'7',
+			'7',
+			'7'
+		]
+	},
+	{
+		name: 'Liquid',
+		photo: 'https://www.ujackets.com/wp-content/uploads/2017/10/U-jackets-174.jpg',
+		scores: [
+			'10',
+			'5',
+			'5',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Solidus',
+		photo: 'https://i.pinimg.com/originals/24/32/59/24325911b260901c0f5414e170234551.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'1',
+			'3'
+		]
+	},
+	{
+		name: 'Liquid Ocelot',
+		photo: 'https://vignette.wikia.nocookie.net/metalgear/images/5/58/LiqOcI1.png/revision/latest?cb=20131210235512',
+		scores: [
+			'5',
+			'4',
+			'4',
+			'2',
+			'4'
+		]
+	}
+	
+];
+
+module.exports = friendsArray;
